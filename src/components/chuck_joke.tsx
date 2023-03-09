0d0a42e03fb0545e763a520a27c1a417034680ed
cj-rupp <72820354+cj-rupp@users.noreply.github.com>
2023-03-09 13:01:21 +0000

    feat: added updates for the first 3 user requirements.

--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -1,9 +1,28 @@
-interface ChuckJokeProps {
+ 
+import { JsxElement } from 'typescript';
+import Joke from '../joke';
 
+interface ChuckJokeProps {
+    jokes: Array<Joke>
 }
 
-const ChuckJoke: React.FC<ChuckJokeProps> = () => <p></p>
+const ChuckJoke: React.FC<ChuckJokeProps> = ({jokes}) => {
+/*    const jokeBlocks:string = jokes.reduce((oldJokes:string,joke:Joke) => {
+        return oldJokes +=`<h3>Joke Number: ${joke.id}</h3><p>${joke.joke}</p>`
+    }, "");
+*/
+    const jokeBlocks:Array<JSX.Element> = jokes.map((joke) => {
+        return(
+            <div>
+            <h3>{`Joke Number: ${joke.id}`}</h3>
+            <p>{joke.joke}</p>
+            </div>
+        )
+    })
 
-
+    return (
+        <p>{jokeBlocks}</p>
+    )
+}
 
 export default ChuckJoke;
