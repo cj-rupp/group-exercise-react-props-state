15eedeab88086810ec4e2afeca82a2d0d9e92762
cj-rupp <72820354+cj-rupp@users.noreply.github.com>
2023-03-09 15:21:12 +0000

    feat: Extended the ChuckJoke component to cover requirement 4 and Extension. I also added a short comment.

--- src/components/chuck_joke.tsx
+++ src/components/chuck_joke.tsx
@@ -1,5 +1,5 @@
  
-import { JsxElement } from 'typescript';
+import { Fragment } from 'react';
 import Joke from '../joke';
 
 interface ChuckJokeProps {
@@ -7,21 +7,32 @@ interface ChuckJokeProps {
 }
 
 const ChuckJoke: React.FC<ChuckJokeProps> = ({jokes}) => {
-/*    const jokeBlocks:string = jokes.reduce((oldJokes:string,joke:Joke) => {
-        return oldJokes +=`<h3>Joke Number: ${joke.id}</h3><p>${joke.joke}</p>`
-    }, "");
-*/
+
+    /* As you can write any JavaScript code you want in the body of a component,
+    as long as it returns a JSX.Element, this component combines requirement 3(/4)
+    and the extension. Fragment is used in the array elements and filtered joke
+    to make it easer to combine the various elements in the return value. */
+    
     const jokeBlocks:Array<JSX.Element> = jokes.map((joke) => {
         return(
-            <div>
+            <Fragment key={joke.id}>
             <h3>{`Joke Number: ${joke.id}`}</h3>
             <p>{joke.joke}</p>
-            </div>
-        )
+            </Fragment>
+        );
     })
 
+    const filteredJokes: Array<Joke> = jokes.filter((joke) => joke.id === 3);
+
+    const filteredBlock: JSX.Element = <Fragment>
+            <h3>Filtered Jokes: </h3>
+            <p>{filteredJokes[0].joke}</p>
+        </Fragment>
+
     return (
-        <p>{jokeBlocks}</p>
+        <div>{jokeBlocks}
+        {filteredBlock}
+        </div>
     )
 }
 
